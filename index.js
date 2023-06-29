23bd71d8c9238b7c23d8cb1e8fe18ce29295615d
velan_s <singaravelan.mr@gmail.com>
2023-06-29 10:15:39 +0530

    newtestforci

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ export const handler = async (event, context) => {
 
   const customKey = event.httpMethod + " " + event.resource;
   const reqBody = Object.keys(event).length > 0 ? JSON.parse(event.body) : event;
-  //   const pathParameter = event?.pathParameters?.id;
+  //   const pathParameter = event?.pathParameters?.i
 
   try {
     switch (customKey) {
